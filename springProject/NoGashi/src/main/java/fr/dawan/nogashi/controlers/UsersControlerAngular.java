89d01949621462f15626eaf31da93e5319137847
Olganix <olganixcorp@gmail.com>
2019-11-17 08:47:58 +0100

    -modif pour faire fonctionner la communication bdd <-> java controller <-> angular

--- springProject/NoGashi/src/main/java/fr/dawan/nogashi/controlers/UsersControlerAngular.java
+++ springProject/NoGashi/src/main/java/fr/dawan/nogashi/controlers/UsersControlerAngular.java
@@ -8,9 +8,12 @@ import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.dawan.nogashi.beans.User;
 import fr.dawan.nogashi.daos.GenericDao;
@@ -18,8 +21,8 @@ import fr.dawan.nogashi.listeners.StartListener;
 
 
 //ici ca ne marcha pas encore.
-@Controller
-//@RestController
+//@Controller
+@RestController
 @RequestMapping("userscontrolerAngular")
 public class UsersControlerAngular {
 	private static final long serialVersionUID = 1L;
@@ -29,10 +32,11 @@ public class UsersControlerAngular {
     }
 
     // Method to test the angular fetch call.
-    //@GetMapping(path="/getUsers")
-    //@CrossOrigin(origins="http://localhost:4200")                           // @CrossOrigin is used to handle the request from a difference origin.
-    @RequestMapping(value= "userscontrolerAngular/getUsers", method= RequestMethod.GET, produces = "application/json")
-    public @ResponseBody List<User> getUsers() 
+    @GetMapping(path="/getUsers", produces = "application/json")
+    @CrossOrigin(origins="http://localhost:4200")                           // @CrossOrigin is used to handle the request from a difference origin.
+    //@RequestMapping(value= "userscontrolerAngular/getUsers", method= RequestMethod.GET, produces = "application/json")
+    //public @ResponseBody List<User> getUsers() 
+    public List<User> getUsers()
     {
     	List<User> listUsers = new ArrayList<User>();
 		
