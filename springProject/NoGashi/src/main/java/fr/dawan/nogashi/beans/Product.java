de9a961439892e5e26f355ae044844aaa83644af
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2019-12-17 15:37:05 +0100

    Ajout du bean Product et enum ProductStatus

---
+++ springProject/NoGashi/src/main/java/fr/dawan/nogashi/beans/Product.java
@@ -0,0 +1,51 @@
+package fr.dawan.nogashi.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import org.springframework.stereotype.Component;
+
+import fr.dawan.nogashi.enums.ProductStatus;
+
+/**
+ * 
+ * @author Joffrey
+ * Instance d'un produit
+ */
+@Entity
+@Component
+public class Product extends ProductTemplate implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private ProductStatus status;
+	private String typedName;
+
+	public Product() {
+	
+	}
+
+	public ProductStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ProductStatus status) {
+		this.status = status;
+	}
+
+	public String getTypedName() {
+		return typedName;
+	}
+
+	public void setTypedName(String typedName) {
+		this.typedName = typedName;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [status=" + status + ", typedName=" + typedName + "]";
+	}
+	
+	
+
+}
