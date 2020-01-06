092cde47b2f491a790c8d45ea602e40afa8a929d
olganix <olganixcorp@gmail.com>
2020-01-06 22:19:34 +0100

    3 valeureses classes faites en une journée ... c'etait un vendredi , l'année derniere

---
+++ springProject/NoGashi/src/main/java/fr/dawan/nogashi/beans/SchedulerDay.java
@@ -0,0 +1,110 @@
+package fr.dawan.nogashi.beans;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+public class SchedulerDay extends DbObject {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Enumerated(EnumType.ORDINAL)
+	private DayOfWeek day = DayOfWeek.MONDAY;
+	
+	@OneToMany(mappedBy = "parent", fetch = FetchType.EAGER)
+	private List<SchedulerHoursRange> hoursRanges = new ArrayList<SchedulerHoursRange>();
+	
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private SchedulerWeek parent;
+	
+	
+	
+	public void addHoursRanges(SchedulerHoursRange hr) {
+		if(! this.hoursRanges.contains(hr))
+		{
+			this.hoursRanges.add(hr);
+			hr.setParent(this);
+		}
+	}
+	
+	
+	
+	//-------------------------------
+	
+
+	public SchedulerDay(DayOfWeek day, SchedulerWeek parent) {
+		super();
+		this.day = day;
+		this.parent = parent;
+	}
+
+
+
+
+	public DayOfWeek getDay() {
+		return day;
+	}
+
+
+
+
+
+	public void setDay(DayOfWeek day) {
+		this.day = day;
+	}
+
+
+
+	public List<SchedulerHoursRange> getHoursRanges() {
+		return hoursRanges;
+	}
+
+
+
+	public void setHoursRanges(List<SchedulerHoursRange> hoursRanges) {
+		this.hoursRanges = hoursRanges;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "SchedulerDay [day=" + day + "]";
+	}
+
+
+
+	public SchedulerDay() {
+		super();
+	}
+
+
+
+	public SchedulerWeek getParent() {
+		return parent;
+	}
+
+
+
+	public void setParent(SchedulerWeek parent) {
+		this.parent = parent;
+	}
+	
+	
+	
+
+}
---
+++ springProject/NoGashi/src/main/java/fr/dawan/nogashi/enums/SchedulerWeekType.java
@@ -0,0 +1,5 @@
+package fr.dawan.nogashi.enums;
+
+public enum SchedulerWeekType {
+	GROUP, OPEN, PRODUCT_PROMOTION, PRODUCT_UNSOLD
+}
