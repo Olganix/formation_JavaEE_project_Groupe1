ffdb3c65ba98938ebabd7f91a508fd51d8f9de69
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-01-12 18:03:39 +0100

    Modification de BuyerController.java
    - Ajout de getCommerceById et getProductById
    - Remplace @RequestMapping par @GetMapping dans getCommerces et getProductsByCommerce
    - Ajout de commentaires

--- springProject/NoGashi/src/main/java/fr/dawan/nogashi/controlers/BuyerController.java
+++ springProject/NoGashi/src/main/java/fr/dawan/nogashi/controlers/BuyerController.java
@@ -6,15 +6,15 @@ import java.util.Locale;
 
 import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
-import javax.persistence.Subgraph;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.dawan.nogashi.beans.Buyer;
@@ -29,12 +29,12 @@ import fr.dawan.nogashi.listeners.StartListener;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200", allowCredentials = "true")                           // @CrossOrigin is used to handle the request from a difference origin.
-public class BuyerController 
+public class BuyerController
 {
 	@Autowired
 	GenericDao dao;
 	
-	
+	// Retourne true si le User de la session est une Association ou un Individual
 	public boolean checkAllowToDoThat(HttpSession session)
 	{
 		User u = (User)session.getAttribute("user");
@@ -47,11 +47,14 @@ public class BuyerController
 	
 	/*****************************************************************************************
 	*										getCommerces									 * 
-	*****************************************************************************************/
-	// Liste tous les commerces
-	@RequestMapping(path="/getCommerces", produces = "application/json")
+	*****************************************************************************************
+	*
+	* Liste tous les commerces
+	*/
+	@GetMapping(path="/commerces", produces = "application/json")
 	public RestResponse<List<Commerce>> getCommerces(HttpSession session)
     {
+		// Check si le User de la session est Association ou Individual
 		if(!checkAllowToDoThat(session))
 			return new RestResponse<List<Commerce>>(RestResponseStatus.FAIL, null, 5, "Error: User is not allowed to do this operation");
 		
@@ -68,6 +71,7 @@ public class BuyerController
     	graph.addSubgraph("products");
     	*/
     	
+    	// Recupere la liste de tous les Commerces
 		try 
 		{	
 			listCommerces = dao.findAll(Commerce.class, em, false, graph);
@@ -81,12 +85,55 @@ public class BuyerController
 	
 	
 	/*****************************************************************************************
-	*										getProducts									 * 
-	*****************************************************************************************/
-	// Liste tous les produits
-	@RequestMapping(path="/getProducts", produces = "application/json")
+	*										getCommerceById									 * 
+	*****************************************************************************************
+	* 
+	* Recupere un Commerce via son id
+	*/
+	@GetMapping(path="/commerces/{id}", produces = "application/json")
+	public RestResponse<Commerce> getCommerceById(@PathVariable(name="id") int id, HttpSession session)
+    {
+		// Check si le User de la session est Association ou Individual
+		if(!checkAllowToDoThat(session))
+			return new RestResponse<Commerce>(RestResponseStatus.FAIL, null, 5, "Error: User is not allowed to perform this operation");
+		
+		
+    	EntityManager em = StartListener.createEntityManager();
+		
+    	Commerce commerce = new Commerce();
+    	
+    	
+    	EntityGraph<Commerce> graph = em.createEntityGraph(Commerce.class);
+    	/*
+    	graph.addSubgraph("commerceCategories");
+    	graph.addSubgraph("productTemplates");
+    	graph.addSubgraph("products");
+    	*/
+    	
+    	// Recupere le Commerce dont l'id est passe en parametre
+		try 
+		{	
+			commerce = dao.find(Commerce.class, id, em, graph);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		em.close();
+		return new RestResponse<Commerce>(RestResponseStatus.SUCCESS, commerce);
+    }
+	
+	
+	
+	/*****************************************************************************************
+	*										getProducts									     * 
+	*****************************************************************************************
+	*
+	* Liste tous les produits
+	*/
+	@GetMapping(path="/products", produces = "application/json")
 	public RestResponse<List<Product>> getProducts(HttpSession session)
     {
+		// Check si le User de la session est Association ou Individual
 		if(!checkAllowToDoThat(session))
 			return new RestResponse<List<Product>>(RestResponseStatus.FAIL, null, 5, "Error: User is not allowed to do this operation");
 		
@@ -103,7 +150,8 @@ public class BuyerController
     	graph.addSubgraph("products");
     	*/
     	
-		try 
+		// Recupere la liste de tous les Products
+    	try 
 		{	
 			listProducts = dao.findAll(Product.class, em, false, graph);
 		} catch (Exception e) {
@@ -118,10 +166,14 @@ public class BuyerController
 	
 	/*****************************************************************************************
 	*										getProductsByCommerce										 * 
-	*****************************************************************************************/
-	@RequestMapping(path="/getProductsByCommerce", produces = "application/json")
-	public RestResponse<List<Product>> getProductsByCommerce(HttpSession session, String idCommerce)
+	*****************************************************************************************
+	*
+	* Liste les Products d'un Commerce entre en parametre
+	*/
+	@GetMapping(path="/commerce/products", produces = "application/json")
+	public RestResponse<List<Product>> getProductsByCommerce(@RequestBody String commerceName, HttpSession session)
     {
+		// Check si le User de la session est Association ou Individual
 		if(!checkAllowToDoThat(session))
 			return new RestResponse<List<Product>>(RestResponseStatus.FAIL, null, 5, "Error: User is not allowed to do this operation");
 		
@@ -130,6 +182,7 @@ public class BuyerController
     	
     	System.out.println( "Test Buyer : " + ((User)session.getAttribute("user")) );
     	
+    	// Recupere le Buyer a partir du User de la session et check si c'est bien ce Buyer qui est connecte
     	Buyer buyer = null;
 		try {
 			buyer = dao.find(Buyer.class, ((User)session.getAttribute("user")).getId() , em);
@@ -150,9 +203,11 @@ public class BuyerController
     	/*Subgraph<Buyer> aa = graph.addSubgraph("buyer", Buyer.class);
     	aa.addSubgraph("commerces");*/
     	
+    	// Recupere la liste des Products du Commerce selectionne
 		try 
 		{	
-			listProducts = dao.findNamed(Product.class, "id", idCommerce, em, true, graph);
+			//listProducts = dao.findBySomethingNamed(Product.class, "commerce", commerce.getName(), em, false, graph);
+			listProducts = dao.findNamed(Product.class, "commerce", commerceName, em, false, graph);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -163,6 +218,44 @@ public class BuyerController
     }
 	
 
+	/*****************************************************************************************
+	*										getProductById									 * 
+	*****************************************************************************************
+	* 
+	* Recupere un Product via son id
+	*/
+	@GetMapping(path="/products/{id}", produces = "application/json")
+	public RestResponse<Product> getProductById(@PathVariable(name="id") int id, HttpSession session)
+    {
+		// Check si le User de la session est Association ou Individual
+		if(!checkAllowToDoThat(session))
+			return new RestResponse<Product>(RestResponseStatus.FAIL, null, 5, "Error: User is not allowed to perform this operation");
+		
+		
+    	EntityManager em = StartListener.createEntityManager();
+		
+    	Product product= new Product();
+    	
+    	
+    	EntityGraph<Product> graph = em.createEntityGraph(Product.class);
+    	/*
+    	graph.addSubgraph("commerceCategories");
+    	graph.addSubgraph("productTemplates");
+    	graph.addSubgraph("products");
+    	*/
+    	
+    	// Recupere le Product dont l'id est passe en parametre
+		try 
+		{	
+			product= dao.find(Product.class, id, em, graph);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		em.close();
+		return new RestResponse<Product>(RestResponseStatus.SUCCESS, product);
+    }
+	
 
 	/*****************************************************************************************
 	*										addProductToCart										 * 
@@ -183,6 +276,7 @@ public class BuyerController
 		
 		return new RestResponse<Product>(RestResponseStatus.SUCCESS, null);
     }
+	
 	
 	
 	
