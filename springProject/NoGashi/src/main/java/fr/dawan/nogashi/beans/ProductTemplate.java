825fbd426b22e32dbe19dcb3e75713962a6a22c2
Anaïs Kordek <kordek.anais@gmail.com>
2020-01-16 09:14:51 +0100

    Ajout de produits pour Big Fernand

--- springProject/NoGashi/src/main/java/fr/dawan/nogashi/beans/ProductTemplate.java
+++ springProject/NoGashi/src/main/java/fr/dawan/nogashi/beans/ProductTemplate.java
@@ -3,7 +3,7 @@ package fr.dawan.nogashi.beans;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
+// import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
