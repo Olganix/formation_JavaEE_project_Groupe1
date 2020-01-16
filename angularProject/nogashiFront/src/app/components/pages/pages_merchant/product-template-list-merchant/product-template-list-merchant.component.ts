5f47528aad049e0d2a7d75cfe445756989be8d8d
Olganix <olganix@hotmail.fr>
2020-01-16 10:13:11 +0100

    decoupe de la page welcome-merchant. dans le but d'afficher la liste des fiches produits.

---
+++ angularProject/nogashiFront/src/app/components/pages/pages_merchant/product-template-list-merchant/product-template-list-merchant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-template-list-merchant',
+  templateUrl: './product-template-list-merchant.component.html',
+  styleUrls: ['./product-template-list-merchant.component.scss']
+})
+export class ProductTemplateListMerchantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angularProject/nogashiFront/src/app/components/pages/pages_merchant/product-template-merchant-view/product-template-merchant-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-template-merchant-view',
+  templateUrl: './product-template-merchant-view.component.html',
+  styleUrls: ['./product-template-merchant-view.component.scss']
+})
+export class ProductTemplateMerchantViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
