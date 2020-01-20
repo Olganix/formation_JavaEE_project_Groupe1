b98ccebc715d9d73c839a118c2e2145b05694a5a
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-01-20 23:42:01 +0100

    Détails ethétiques
    - intégration d'une google map
    - fixe les boutons-up des pages welcome

--- angularProject/nogashiFront/src/app/components/pages/pages_individual/welcome-individual/welcome-individual.component.ts
+++ angularProject/nogashiFront/src/app/components/pages/pages_individual/welcome-individual/welcome-individual.component.ts
@@ -14,4 +14,14 @@ export class WelcomeIndividualComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollToTop() {
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 250); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  }
 }
